0ffd7ca3bd27e4373a0ade67fd503c6950c1db21
shaneemichael <shanemichaeltanata@gmail.com>
2024-12-20 11:24:56 +0700

    Initialize Base Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
-import 'package:manganrek_mobile/screens/homepage.dart';
+import 'package:manganrek_mobile/restoran_makanan/screens/homepage.dart';
 
 void main() {
   runApp(const MyApp());
